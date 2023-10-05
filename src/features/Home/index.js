73e154768d42ffb98248fe7a478683f2585d5f43
swingislee <swingislee@gmail.com>
2023-10-05 14:45:23 +0800

    correct ico and title

--- src/features/Home/index.js
+++ src/features/Home/index.js
@@ -1,8 +1,6 @@
 //  \src\features\Home\index.js
 
 import React from 'react';
-import { motion } from 'framer-motion';
-import styles from './styles/HomeIndex.css'
 
 const HomePage = () => {
     return (
